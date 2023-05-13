c0e83a0461fe1096a3dd397971b956504b069d29
Shane <gupitshanehazel@gmail.com>
2023-05-13 08:41:35 +0800

    pseudocode

---
+++ Simple App Calculator.py
@@ -0,0 +1,31 @@
+# pseudocode
+
+# make a loop for trying the program again option
+
+    # ask user for their desired operation
+
+    # check if the inputs are the given operations
+        
+        # see if the user inputted the correct value
+
+        # if not, print the error phrase and will make the user type values again
+
+        # make addition operation
+
+        # make subtraction operation
+        
+        # make multiplication operation
+
+        # make division operation
+
+            # ask the user if they want the form, decimal or fraction
+
+            # make division operation with decimal answer
+
+            # make division operation with fraction answer
+
+        # ask the user if they want to calculate again
+
+        # if no, break the loop
+    
+    # make the user input again if they inputted wrong string
